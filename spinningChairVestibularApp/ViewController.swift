c662ab39e975b760b5f46fa7a27b05999adf81cf
Arlo Erdaka <arlo.erdaka@gmail.com>
2019-07-18 16:08:13 +0700

    Initial Commit

---
+++ spinningChairVestibularApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spinningChairVestibularApp
+//
+//  Created by Arlo Erdaka on 18/07/19.
+//  Copyright © 2019 Arlo Erdaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
